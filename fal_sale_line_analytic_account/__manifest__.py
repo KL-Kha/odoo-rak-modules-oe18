1785c7f94e342bbf854225f5ac8450fa33f69d60
VTV24710 <trungvyvo123@gmail.com>
2025-04-03 10:16:54 +0700

    migrate fal_sale_line_analytic_account

---
+++ fal_sale_line_analytic_account/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Analytic Account On Sale Order Line",
+    "version": "18.0.0.1",
+    'license': 'OPL-1',
+    'summary': 'Add analytic account on sale order line',
+    'category': 'Sales',
+    'author': "CLuedoo",
+    'website': "https://www.cluedoo.com",
+    'support': 'cluedoo@falinwa.com',
+    "description": """
+        Add analytic account on sale order line
+    """,
+    "depends": [
+        'sale',
+        'purchase_stock',
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'images': [
+    ],
+    'demo': [
+    ],
+    'price': 180.00,
+    'currency': 'EUR',
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4
---
+++ fal_sale_line_analytic_account/models/sale_order.py
@@ -0,0 +1,50 @@
+from odoo import models, fields, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    fal_analytic_account_id = fields.Many2one(
+        'account.analytic.account', string='Analytic Account', copy=False)
+
+    # transfer analytic account to invoice
+    def _prepare_invoice_line(self, **optional_values):
+        res = super(SaleOrderLine, self)._prepare_invoice_line(**optional_values)
+        res['analytic_account_id'] = self.fal_analytic_account_id.id or self.order_id.analytic_account_id.id
+        return res
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    # Transfer analytic account to purchase "_run_buy"
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(self, product_id, product_qty, product_uom, company_id, values, po):
+        res = super(PurchaseOrderLine, self)._prepare_purchase_order_line_from_procurement(product_id, product_qty, product_uom, company_id, values, po)
+        move = values.get('move_dest_ids', False)
+        sale_line = move and move.sale_line_id
+        sale_line_obj = self.env['sale.order.line']
+        analytic = False
+        if sale_line:
+            analytic = sale_line.fal_analytic_account_id.id or sale_line.order_id.analytic_account_id.id
+        # for drop shipping
+        elif values.get('sale_line_id', False):
+            sale_line_id = sale_line_obj.browse(values.get('sale_line_id', False))
+            analytic = sale_line_id.fal_analytic_account_id.id or sale_line_id.order_id.analytic_account_id.id
+        res['account_analytic_id'] = analytic
+        return res
+
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    # update Analytic account
+    def _update_purchase_order_line(self, product_id, product_qty, product_uom, company_id, values, line):
+        res = super(StockRule, self)._update_purchase_order_line(product_id, product_qty, product_uom, company_id, values, line)
+        move = values.get('move_dest_ids')
+        sale_line = move and move.sale_line_id
+        analytic = False
+        if sale_line:
+            analytic = sale_line.fal_analytic_account_id.id or sale_line.order_id.analytic_account_id.id
+        res['account_analytic_id'] = analytic
+        return res
