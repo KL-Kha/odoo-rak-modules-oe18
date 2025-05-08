032ac0061d38ea7c5642983041215057904797c6
Trần Bảo Kha <baokha0009@gmail.com>
2025-05-08 08:25:16 +0000

    remove_tax_on_portal

---
+++ remove_tax_on_portal/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Remove tax on portal',
+    'version': '1.0',
+    'category': 'Sale',
+    'summary': 'Remove tax on portal',
+    'description': """
+Remove tax on portal
+======================================.
+    """,
+    'depends': ['base','website_sale','sale_management'],
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'demo':[
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
