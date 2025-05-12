c3cf05430edc3a2b9c57f3b4b3212221200deed1
Trần Bảo Kha <72544318+KL-Kha@users.noreply.github.com>
2025-05-12 11:55:51 +0200

    Update stock_picking_type.py

--- mjb_order_qty_done_limit/models/stock_picking_type.py
+++ mjb_order_qty_done_limit/models/stock_picking_type.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
-    x_picking_qty_done_limit = fields.One2many('x_order_qty_done_limit', 'x_picking_type_id', string='Picking Quantity Done Limits')
+    x_order_qty_done_limit = fields.One2many('x_order_qty_done_limit', 'x_picking_type_id', string='Picking Quantity Done Limits')
