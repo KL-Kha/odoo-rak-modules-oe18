fa50270237f274f1091ddbdefaf71a50409fc640
VietNgoMJB <vn@majorbird.cn>
2025-05-12 14:36:06 +0800

    Update __manifest__.py

--- remove_tax_on_portal/__manifest__.py
+++ remove_tax_on_portal/__manifest__.py
@@ -8,7 +8,7 @@
 Remove tax on portal
 ======================================.
     """,
-    'depends': ['base','website_sale','sale_management'],
+    'depends': ['base', 'sale','website_sale','sale_management'],
     'data': [
         'views/sale_view.xml',
     ],
