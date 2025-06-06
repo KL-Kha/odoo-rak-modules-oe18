295b23679dee054d2fdf6bf3146dbeda78ba658d
VietNgoMJB <vn@majorbird.cn>
2025-06-06 16:26:43 +0800

    Update manufacturing.py

--- highlight_out_of_stock_quantity/models/manufacturing.py
+++ highlight_out_of_stock_quantity/models/manufacturing.py
@@ -7,12 +7,15 @@ from odoo import fields, models, api, _
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    @api.depends('picking_ids.name', 'picking_ids.state')
     def get_trans_detail(self):
         state_selection = dict(self.env['stock.picking'].fields_get(allfields=['state'])['state']['selection'])
         for rec in self:
             html = '<p>'
-            for one_line in rec.picking_ids:
+            picking_ids = self.env['stock.picking'].search([
+                ('group_id', '=', rec.procurement_group_id.id), ('group_id', '!=', False),
+            ])
+            picking_ids |= rec.move_raw_ids.move_orig_ids.picking_id
+            for one_line in picking_ids:
                 state_label = state_selection.get(one_line.state, one_line.state)
                 html += '<b>' + one_line.name + '</b>: ' + state_label + '<br/>'
             html += '</p>'
