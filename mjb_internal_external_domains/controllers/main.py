11556731084aed92c4cf1631ed8370f73d29bce6
VietNgoMJB <164287187+VietNgoMJB@users.noreply.github.com>
2025-08-10 11:57:11 +0800

    Update main.py

--- mjb_internal_external_domains/controllers/main.py
+++ mjb_internal_external_domains/controllers/main.py
@@ -34,9 +34,9 @@ def checkAccess(_url, _user, _env, pageType='portal'):
             accessGranted
         ])
         return True
-    return True
-    externalDomain = "erp-stg.rak-internal.net"
-    internalDomain = "erp-stg.rak-internal.net"
+    
+    # externalDomain = "erp-stg.rak-internal.net"
+    # internalDomain = "erp-stg.rak-internal.net"
 
     isInternalDomain = internalDomain in _url
     isExternalDomain = externalDomain in _url
